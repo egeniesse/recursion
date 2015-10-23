0e12dfdf6ff506a1b2d01e560e891fa9a5cdb48c
Eric Geniesse <egeniesse@gmail.com>
2015-10-23 07:39:24 -0700

    Added the get elements by class name function

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,36 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
-  // your code here
+var getElementsByClassName = function(className){
+
+	var passingElements = [];
+
+	function walkTheDom(selectedNode, className){
+
+
+			var children = selectedNode.childNodes; 
+			var classArr = selectedNode.classList;
+
+			if(classArr){ // Determines if the selected element has any class attributes
+
+				_.each(classArr, function(element){ 
+					if (element === className){  
+						passingElements.push(selectedNode);  // If any of the classes match the desired class name, the element is pushed to the passingElements array
+					}  
+				});
+			}
+
+			if (children){ // Determines if the selected element has any classArr.
+
+				_.each(children, function(element){ //recursively calls walkTheDom on each child
+					walkTheDom(element, className)
+				});
+				
+			}	
+	};
+
+	walkTheDom(document.body, className); 
+	
+	return passingElements;
+  
 };
